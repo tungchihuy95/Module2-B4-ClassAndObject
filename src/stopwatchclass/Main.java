50ab5e3e0f9e7e9aa33659762f763c184cdab81c
tungchihuy95 <tungjpvn95@gmail.com>
2020-09-14 16:55:24 +0700

    stopwatch

---
+++ src/stopwatchclass/Main.java
@@ -0,0 +1,17 @@
+package stopwatchclass;
+
+public class Main {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        for (int i = 0; i < 100000 ; i++) {
+            System.out.println(i);
+        }
+        stopWatch.end();
+
+        double c = stopWatch.getElapsedTime();
+
+        System.out.println(c);
+    }
+}
--- src/stopwatchclass/StopWatch.java
+++ src/stopwatchclass/StopWatch.java
@@ -1,5 +1,21 @@
 package stopwatchclass;
 
 public class StopWatch {
+    private double startTime, endTime;
 
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void end() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public double getElapsedTime() {
+        return this.endTime - this.startTime;
+    }
 }
