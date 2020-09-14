b9a702841817351c9a1d751335aba97da5025a6e
tungchihuy95 <tungjpvn95@gmail.com>
2020-09-14 20:58:27 +0700

    change

--- src/stopwatchclass/Main.java
+++ src/stopwatchclass/Main.java
@@ -5,8 +5,26 @@ public class Main {
         StopWatch stopWatch = new StopWatch();
 
         stopWatch.start();
-        for (int i = 0; i < 100000 ; i++) {
-            System.out.println(i);
+//        for (int i = 0; i < 100000 ; i++) {
+//            System.out.println(i);
+
+        int []array = new int[100000];
+
+        //cac gia tri tai "i" cua mang array
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000 + 1); //vi chay tu 0 den 99999 nen phai cong them 1
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length -1; j++) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
         stopWatch.end();
 
